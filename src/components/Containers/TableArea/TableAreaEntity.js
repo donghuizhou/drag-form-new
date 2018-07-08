41d2e0fba2af4a15ce50aebdd7c141473a163f15
80249965/周东晖 <80249965@itc.cmbchina.cn>
2018-07-08 21:31:13 +0800

    commit

--- src/components/Containers/TableArea/TableAreaEntity.js
+++ src/components/Containers/TableArea/TableAreaEntity.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Table, Divider } from 'antd';
 import store from '../../../redux/store';
 import { updateCurActiveItem, updateCurActiveTab } from '../../../redux/actions';
+import './tableStyle.css';
 
 class TableAreaEntity extends Component {
   tableWrapClick = (e) => {
@@ -16,7 +17,7 @@ class TableAreaEntity extends Component {
       { title: 'city', dataIndex: 'city', key: 'city' }
     ];
     return (
-      <div onClick={this.tableWrapClick}>
+      <div onClick={this.tableWrapClick} class="tableWrap">
         <Table id={this.props.id} columns={columns}></Table>
       </div>
     );
@@ -24,3 +25,30 @@ class TableAreaEntity extends Component {
 }
 
 export default TableAreaEntity;
+
+[
+  {
+    type: 'SearchArea',
+    attrs: {},
+    children: [
+      { type: 'input', attrs: {}, children: '' },
+      { type: 'input', attrs: {}, children: '' },
+      { type: 'input', attrs: {}, children: '' },
+      { type: 'select', attrs: {}, children: '' }
+    ]
+  },
+  {
+    type: 'ButtonArea',
+    attrs: {},
+    children: [
+      { type: 'button', attrs: {}, children: '' },
+      { type: 'button', attrs: {}, children: '' },
+      { type: 'button', attrs: {}, children: '' }
+    ]
+  },
+  {
+    type: 'TableArea',
+    attrs: {},
+    children: ''
+  }
+]
